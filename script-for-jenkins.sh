2b167ae358bdc441a9505128c6ec4eb311a5c030
Patel , Pritesh <pritesh.patel@synechron.com>
2018-12-19 15:58:22 +0530

    demo for jenkins

---
+++ script-for-jenkins.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Hello"
+echo "[INFO] sending data to dmp file"
+
+
+echo "***************************"
+
+echo "printing value for server name ${servername}"
+
+echo "***************************"
